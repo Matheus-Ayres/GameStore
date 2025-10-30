a347f2c5010af57ea95b76b08829ce3f1877ee06
Matheus-Ayres <matheussayres9@gmail.com>
2025-10-30 15:30:12 -0300

    details in cartPage

--- src/components/itemInCartComponent.vue
+++ src/components/itemInCartComponent.vue
@@ -23,31 +23,35 @@ function decrementQuantity() {
 </script>
 
 <template>
-    
+
     <div class="flex items-center bg-gray-50 rounded-lg p-4 shadow-sm">
-        <img class="w-24 h-auto rounded-md mr-4" :src="image"
-            alt="Xbox Series X">
+        <img class="w-24 h-auto rounded-md mr-4" :src="image" alt="Xbox Series X">
         <div class="info">
-            <h2 class="text-lg text-gray-800">{{name}}</h2>
-            <p class="text-red-600 font-bold mt-1">R${{price}}</p>
+            <h2 class="text-lg text-gray-800">{{ name }}</h2>
+            <p class="text-red-600 font-bold mt-1">R${{ price }}</p>
             <div class="text-sm">
                 <span>
-                    Quantidade 
+                    Quantidade
                 </span>
 
-                <section class=" flex items-center justify-bewtween gap-4 mt-2">
-                    <div class="bg-red-600 text-white font-bold p-1 rounded-l-full cursor-pointer" @click="decrementQuantity">
-                        -
+                <section class="flex items-center justify-between gap-4 mt-2 w-80">
+                    <div class="flex items-center gap-3">
+                        <div class="bg-red-600 text-white font-bold px-2 py-1 rounded-l-full cursor-pointer select-none"
+                            @click="decrementQuantity">
+                            -
+                        </div>
+                        <div>{{ quantityInCard }}</div>
+                        <div class="bg-red-600 text-white font-bold px-2 py-1 rounded-r-full cursor-pointer select-none"
+                            @click="incrementQuantity">
+                            +
+                        </div>
                     </div>
-                    <div class="">
-                        {{ quantityInCard }}
-                    </div>
-                    <div class="bg-red-600 text-white font-bold p-1 rounded-r-full cursor-pointer" @click="incrementQuantity">
-                        +
-                    </div>
+
+                    <img src="/src/assets/icons/lixoPreto.png" class="cursor-pointer" />
                 </section>
+
             </div>
         </div>
-        
+
     </div>
 </template>
